36c0d07b3a286c9fde36b1e8f839597e1a14bc42
MattSams420 <56139555+MattSams420@users.noreply.github.com>
2020-06-24 10:31:55 +0900

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+//render default page
 router.get("/", (req,res) => {
     //res.render("index", {data: "hello"});
-    res.render("index", {file: ""});
+    res.render("index", {
+        file: ""
+    });
     
 })
 
-
-
-
+//download file method
 router.get('/download', (req,res) => {
     const file = `test.txt`
     res.download(file);
